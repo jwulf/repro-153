ac4002bebc21b599309f040895e5f1e73cc29ebf
Josh Wulf <josh.wulf@camunda.com>
2024-05-08 11:00:54 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const camundaSdk = require('@camunda8/sdk');
+// Incorrect:
+/**
 const camunda = new camundaSdk.Camunda8({
   config: {
     ZEEBE_ADDRESS: 'zeebeAddress',
     ZEEBE_CLIENT_ID: 'camundaClientId',
     ZEEBE_CLIENT_SECRET: 'camundaClientSecret',
-//    ZEEBE_AUTHORIZATION_SERVER_URL: 'https://login.cloud.camunda.io/oauth/token',
+    ZEEBE_AUTHORIZATION_SERVER_URL: 'https://login.cloud.camunda.io/oauth/token',
     CAMUNDA_OAUTH_URL: 'https://login.cloud.camunda.io/oauth/token'
   }
+});
+**/
+
+
+// Should be:
+const camunda = new camundaSdk.Camunda8({
+    ZEEBE_ADDRESS: 'zeebeAddress',
+    ZEEBE_CLIENT_ID: 'camundaClientId',
+    ZEEBE_CLIENT_SECRET: 'camundaClientSecret',
+    ZEEBE_AUTHORIZATION_SERVER_URL: 'https://login.cloud.camunda.io/oauth/token',
+    CAMUNDA_OAUTH_URL: 'https://login.cloud.camunda.io/oauth/token'
 });
