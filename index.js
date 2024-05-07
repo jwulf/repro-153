7108c8b78c53b454e6dafa2beec1b55d5be89cb7
Josh Wulf <josh.wulf@camunda.com>
2024-05-07 19:49:25 +1200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const camundaSdk = require('@camunda8/sdk');
+const camunda = new camundaSdk.Camunda8({
+  config: {
+    ZEEBE_ADDRESS: 'zeebeAddress',
+    ZEEBE_CLIENT_ID: 'camundaClientId',
+    ZEEBE_CLIENT_SECRET: 'camundaClientSecret',
+//    ZEEBE_AUTHORIZATION_SERVER_URL: 'https://login.cloud.camunda.io/oauth/token',
+    CAMUNDA_OAUTH_URL: 'https://login.cloud.camunda.io/oauth/token'
+  }
+});
